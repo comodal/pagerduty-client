734d900904121dc0d5e3ddb79c6d73009342c4c7
James Edwards <james.p.edwards42@gmail.com>
2018-08-01 09:31:27 -0500

    Init project structure.

---
+++ systems.comodal.pagerduty_event_client/src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module systems.comodal.pagerduty_event_client {
+  exports systems.comodal.pagerduty.event.client;
+}
---
+++ systems.comodal.pagerduty_event_client_test/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module systems.comodal.pagerduty_event_client_test {
+  requires transitive systems.comodal.pagerduty_event_client;
+  requires transitive jdk.httpserver;
+  requires org.junit.jupiter.api;
+  requires org.opentest4j;
+}
